9c844ef096daf0f4d04d4d2105d52ad31dcc8eeb
Shirazul Haque <mshaque070@gmail.com>
2021-11-06 17:04:03 +0000

    Update commit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var playerCard = "card5"
+    @State var cpuCard = "card9"
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
     var body: some View {
-        
-        var playerCard = "card5"
-        var cpuCard = "card9"
-        var playerScore = 0
-        var cpuScore = 0
+    
         
         ZStack {
             Image("background").ignoresSafeArea()
@@ -33,10 +35,27 @@ struct ContentView: View {
 
 
                 Button (action:{
+                    
+                    //Generate a random number between 2 and 14
+                    let playerRand = Int.random(in:2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
                     // update the cards
-                    
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
                     //update the score
-                    
+                    if playerRand > cpuRand
+                    {
+                        playerScore += 1
+                    }
+                    else if cpuRand > playerRand
+                    {
+                        cpuScore += 1
+                    }
+                    else{
+                        cpuScore += 1
+                        playerScore += 1
+                    }
                 }, label: {
                     Image("dealbutton")
 
